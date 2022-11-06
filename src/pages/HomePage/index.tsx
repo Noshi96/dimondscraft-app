af1b0f8e2e5b6911efea88de9cc509873d97df24
Noshi96 <2spawel2@gmail.com>
2022-11-06 20:55:14 +0100

    DL-14 Adjust animated text to mobile

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -13,9 +13,9 @@ const HomePage = () => {
     <>
       <PageWrapper>
         <MainHome height={height}>
-          <MyTyping>
-            <Ttyping>|We Forge The Future|</Ttyping>
-          </MyTyping>
+          <TextWrapper>
+            <AnimatedText>|We Forge The Future|</AnimatedText>
+          </TextWrapper>
           <Image
             src={backgroundPhoto}
             alt='logo'
@@ -34,6 +34,48 @@ const HomePage = () => {
   )
 }
 
+const MainHome = styled.div<{ height: string }>`
+  width: 100%;
+  height: ${(props) => props.height};
+  position: relative;
+`
+
+const TextWrapper = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+`
+
+const typing = keyframes`
+  from {background-size:0 200%; width: 0}
+`
+
+const blink = keyframes`
+ 50% {background-position:0 -100%,0 0}
+`
+
+const AnimatedText = styled.span`
+  text-transform: uppercase;
+  font-weight: 600;
+  font-size: 3rem;
+  --n: 23;
+  color: transparent;
+  background: linear-gradient(-90deg, #ffffff 5px, #0000 0) 10px 0,
+    linear-gradient(#ffffff 0 0) 0 0;
+  background-size: calc(var(--n) * 1ch) 200%;
+  -webkit-background-clip: padding-box, text;
+  background-clip: padding-box, text;
+  background-repeat: no-repeat;
+  animation: ${blink} 0.7s infinite steps(1),
+    ${typing} calc(var(--n) * 0.2s) steps(var(--n)) forwards;
+
+  @media only screen and (min-width: 600px) {
+    font-size: 4rem;
+  }
+`
+
 const Image = styled.img`
   z-index: -1;
   position: absolute;
@@ -41,99 +83,6 @@ const Image = styled.img`
   left: 0;
   object-fit: cover;
   object-position: 70% 0%;
-`
-const typing = keyframes`
-  from {
-    width: 0;
-  }
-`
-const blink = keyframes`
-  50%{
-    border-color: transparent;
-  }
-`
-const effect = keyframes`
-  60% {
-      border-color: transparent
-  }
-`
-
-const AnimatedText = styled.h1`
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  color: #ffffff;
-  width: 100%;
-  animation: ${typing} 2s steps(18), ${blink} 1s step-end infinite alternate;
-  text-align: center;
-  white-space:normal;
-  overflow: hidden;
-  border-right: 3px solid;
-  font-size: 3rem;
-  text-transform: uppercase;
-
-  /* position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  color: #ffffff;
-  width: 23ch;
-  animation: ${typing} 2s steps(18), ${blink} 1s step-end infinite alternate;
-  white-space: nowrap;
-  overflow: hidden;
-  border-right: 3px solid;
-  font-size: 4em;
-  text-transform: uppercase; */
-`
-
-const MyTyping = styled.div`
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-`
-const MyEffect = styled.div`
-  color: white;
-  width: 42ch;
-  animation: ${typing} 2s steps(25), ${effect} 0.5s step-end infinite alternate;
-  white-space: nowrap;
-  overflow: hidden;
-  border-right: 3px solid;
-  font-family: monospace;
-  font-size: 2em;
-`
-
-const HeadLineText = styled.h2`
-  font-size: 3rem;
-`
-
-const MainHome = styled.div<{ height: string }>`
-  width: 100%;
-  height: ${(props) => props.height};
-  position: relative;
-`
-
-const t = keyframes`
-  from {background-size:0 200%; width: 0}
-`
-
-const b = keyframes`
- 50% {background-position:0 -100%,0 0}
-`
-
-const Ttyping = styled.span`
-  --n: 53;
-  color: transparent;
-  background: linear-gradient(-90deg, #00dffc 5px, #0000 0) 10px 0,
-    linear-gradient(#00dffc 0 0) 0 0;
-  background-size: calc(var(--n) * 1ch) 200%;
-  -webkit-background-clip: padding-box, text;
-  background-clip: padding-box, text;
-  background-repeat: no-repeat;
-  animation: ${b} 0.7s infinite steps(1),
-    ${t} calc(var(--n) * 0.3s) steps(var(--n)) forwards;
 `
 
 export default HomePage
