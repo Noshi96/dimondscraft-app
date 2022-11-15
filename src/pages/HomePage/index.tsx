3ef2ad43537ec5b918cadef33deafdf9d8dbef5a
Noshi96 <2spawel2@gmail.com>
2022-11-15 20:16:08 +0100

    DL-22 Add hero section to root

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -2,14 +2,12 @@ import { EventSection } from '../../features/EventSection'
 import { PageWrapper } from '../../layouts/PageWrapper'
 import ArtistsSection from '../../features/ArtistsSection'
 import PartnershipSection from '../../features/PartnershipSection'
-import EntrySection from '../../features/EntrySection'
-import KnowUsBetterSection from "../../features/KnowUsBetterSection";
+import KnowUsBetterSection from '../../features/KnowUsBetterSection'
 
 const HomePage = () => {
   return (
     <>
       <PageWrapper>
-        <EntrySection />
         <KnowUsBetterSection />
         <EventSection />
         <PartnershipSection sectionTitle={'Partnerzy'} />
--- src/pages/RootPage/index.tsx
+++ src/pages/RootPage/index.tsx
@@ -4,6 +4,7 @@ import { GlobalStyle } from '../../styles/global-styles'
 import { useOutlet } from 'react-router-dom'
 import { ScrollRestoration } from 'react-router-dom'
 import Footer from '../../features/Footer'
+import EntrySection from '../../features/EntrySection'
 const HomePage = React.lazy(() => import('../../pages/HomePage'))
 
 const suspenseComponent = (component: React.ReactNode) => {
@@ -16,6 +17,7 @@ const RootPage = () => {
   return (
     <>
       <NavBar />
+      <EntrySection />
       {outlet || suspenseComponent(<HomePage />)}
       <Footer />
       <GlobalStyle />
